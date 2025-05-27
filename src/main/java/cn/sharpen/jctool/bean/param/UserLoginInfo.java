21fcc5c563d198859f67665adf290cd15ea368a5
vun <vunyunt@gmail.com>
2025-05-27 17:49:10 +0800

    fix: UserLoginInfo 添加字段

--- src/main/java/cn/sharpen/jctool/bean/param/UserLoginInfo.java
+++ src/main/java/cn/sharpen/jctool/bean/param/UserLoginInfo.java
@@ -20,33 +20,44 @@ import java.util.Map;
 @Data
 public class UserLoginInfo {
 
-  /**
-   * 用户ID
-   */
-  private String id=null;
-  /**
-   * 用户手机号
-   */
-  private String cellphone=null;
-  /**
-   * 用户类型
-   */
-  private String category=null;
-  /**
-   * 用户账户
-   */
-  private String loginAccount=null;
-  /**
-   * 用户昵称
-   */
-  private String nickname = null;
+    /**
+     * 用户ID
+     */
+    private String id = null;
+
+    /**
+     * 手机区域码
+     */
+    private String areaCode = null;
+
+    /**
+     * 用户手机号
+     */
+    private String cellphone = null;
+    /**
+     * 用户类型
+     */
+    private String category = null;
+    /**
+     * 用户账户
+     */
+    private String loginAccount = null;
+    /**
+     * 用户昵称
+     */
+    private String nickname = null;
+
+    /**
+     * 填写描述，用户填写
+     */
+    private String dataDescribe = null;
 
 
-  /**
-   * 扩展参数
-   */
-  private Map extParam=ExtMap.inst().get();
+    /**
+     * 扩展参数
+     */
+    private Map extParam = ExtMap.inst().get();
 
-  public static final long serialVersionUID = 203L;
+    public static final long serialVersionUID = 203L;
 
 }
